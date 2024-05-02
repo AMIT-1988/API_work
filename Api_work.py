90164f2be86e07acbbad4469516605a20844f0be
AMIT KUMAR DEY <amit_cse43@yahoo.com>
2024-05-02 13:36:03 +0200

    Api_dada_request

--- Api_work.py
+++ Api_work.py
@@ -1,1 +1,18 @@
-print 
+import requests
+
+# Define the API endpoint URL
+url = "https://api.sportmonks.com/v3/core/countries/1161?api_token=MeXYvpmiUUDCyK2p5LIO2hZfn5FBzxMzQWAeTxFz5dEpz3T4igMtML9dMZ4J"
+
+# Make a GET request to the API endpoint
+response = requests.get(url)
+
+# Check if the request was successful (status code 200)
+if response.status_code == 200:
+    # Parse the JSON response data
+    data = response.json()
+    
+    # Do something with the data
+    print(data)
+else:
+    # Print an error message if the request was not successful
+    print(f"Error: {response.status_code}")
